3f57b578a556483b8e653e6b0b494447a84bde9b
Susa1ev <susalytt@gmail.com>
2021-11-30 22:01:13 +0300

    Добавьте файлы проекта.

---
+++ prac3.2(c)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace prac3._2_c_
+{
+    class prac
+    {
+        public prac()
+        {
+            Main();
+        }
+        int a, b, h;
+        double f(double x)
+        {
+            if(x==1||x==-1)
+            {
+                return 1;
+            }else if (x>=0&&x!=1)
+            {
+                return -1 / (1 - x);
+            }else
+            {
+                return 1 / (1 + x);
+            }
+        }
+        void Main()
+        {
+        reTry:
+            Console.WriteLine("Введите a,b,h:");
+            if( Int32.TryParse(Console.ReadLine(), out a) &&
+                Int32.TryParse(Console.ReadLine(), out b) &&
+                Int32.TryParse(Console.ReadLine(), out h))
+            {
+                for (int i = a; i < b+1; i+=h)
+                {
+                    Console.WriteLine(i+" - "+f(i));
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели что-то не то, попробуйте ещё раз");
+                goto reTry;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
